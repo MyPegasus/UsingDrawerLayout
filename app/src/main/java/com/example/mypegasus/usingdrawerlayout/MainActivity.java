8370529797ae59ea34d4b2de08bcc85ac2a39db2
FLYiNg_hbt <flying_hbt@163.com>
2015-08-28 18:23:37 +0800

    Android 侧滑菜单之 DrawerLayout 的使用--1.2.3.4.5 2. 创建抽屉布局 3. 初始化导航列表 4. 监听抽屉的打开关闭事件 5. 点击图标开闭抽屉

--- app/src/main/java/com/example/mypegasus/usingdrawerlayout/MainActivity.java
+++ app/src/main/java/com/example/mypegasus/usingdrawerlayout/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.mypegasus.usingdrawerlayout;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,11 +24,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 	private ListView mDrawerList;
 	private ArrayList<String> menuLists;
 	private ArrayAdapter<String> adapter;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private String mTitle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mTitle = (String) getTitle();
 
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		mDrawerList = (ListView) findViewById(R.id.left_drawer);
@@ -35,6 +43,43 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuLists);
 		mDrawerList.setAdapter(adapter);
 		mDrawerList.setOnItemClickListener(this);
+		// v7的 ActionBarDrawerToggle 没有这个构造方法
+//		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close);
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+				//getActionBar().setTitle("请选择");
+//				setTitle("请选择");
+				getSupportActionBar().setTitle("请选择");
+				invalidateOptionsMenu(); // Call onPrepareOptionsMenu()
+			}
+
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				super.onDrawerClosed(drawerView);
+				//getActionBar().setTitle(mTitle);
+//				setTitle(mTitle);
+				getSupportActionBar().setTitle(mTitle);
+				invalidateOptionsMenu();
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+		// 开启 ActionBar 上的 App Icon 功能
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+	}
+
+	/**
+	 * 调用 invalidateOptionsMenu() 方法时，系统调用该方法
+	 * */
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		boolean isDrawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+		menu.findItem(R.id.action_webSearch).setVisible(!isDrawerOpen);
+		return super.onPrepareOptionsMenu(menu);
 	}
 
 	@Override
@@ -52,11 +97,40 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 		int id = item.getItemId();
 
 		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
+		/*if (id == R.id.action_settings) {
 			return true;
+		}*/
+
+		// 将ActionBar上的图标与Drawer结合
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		switch (id) {
+			case R.id.action_webSearch:
+//				Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.jikexueyuan.com"));
+				Intent i = new Intent();
+				i.setAction("android.intent.action.VIEW");
+				Uri uri = Uri.parse("http://www.jikexueyuan.com");
+				i.setData(uri);
+				startActivity(i);
+				break;
 		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// 需要将ActionBarDrawerToggle与DrawerLayout的状态同步
+		// 将ActionBarDrawerToggle中的drawer图标，设置为ActionBar中的Home-Button 的 Icon
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
 	}
 
 	@Override
